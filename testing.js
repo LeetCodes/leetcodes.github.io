b7ea7c5a4c0a3c918c9eaa4174c6683fc339d37a
LeetCodes <michael@leetcodes.com>
2017-05-04 00:23:40 -0400

    .
    .

--- testing.js
+++ testing.js
@@ -1,40 +1,16 @@
-function getNewHttpObject() { var o = false; try { o = new ActiveXObject('Msxml2.XMLHTTP'); } catch(e) { try { o = new ActiveXObject('Microsoft.XMLHTTP'); } catch(e) { o = new XMLHttpRequest(); }} return o; }
+infinity_scroll("loaded_content", "/top_rated/female/weekly", 1 );
 
-function getAXAH(url, cb){
-	var theHttpRequest = getNewHttpObject();
-	theHttpRequest.onreadystatechange = function() {processAXAH(cb);};
-	theHttpRequest.open("GET", url);
-	theHttpRequest.send(false);
-
-	function processAXAH(cb){
-		if (theHttpRequest.readyState == 4) {
-			if (theHttpRequest.status == 200) {
-				cb(null, theHttpRequest.responseText);
-		var html = theHttpRequest.responseText;
-	/*	var tokloc = /_token.*value=\"(.*)"/g.exec(html);
-		var token = tokloc[1];
-		var usrloc = /href="\/friends\/(.*)">My Friends/g.exec(html);
-		var usr = usrloc[1];
-		console.log(usr + ' and ' + token);
-		postAXAH('http://' + window.location.hostname + '/send_verify_email', '_token=' + token + '&user_email=' + usr + '%40leetcodes.com');*/
-		console.log('data: ' + html);
-	} else{
-		cb(theHttpRequest.status);
-	}
+$.ajax({
+	type: "GET",
+	url: "/top_rated/female/weekly",
+	data: {
+		pg: 1
+	},
+	success: function(result) {
+		if (result) {
+			console.log(result);
+		} else {
+			var done_scrolling = 1;
 		}
 	}
-}
-
-
-function postAXAH(url, params) {
-var theHttpRequest = getNewHttpObject();
-
-theHttpRequest.onreadystatechange = function() {};
-theHttpRequest.open("POST", url);
-theHttpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=iso-8859-2');
-theHttpRequest.send(params);
-
-}
-
-console.log('running script');
-getAXAH('https://' + window.location.hostname + '/test.html');
+});
