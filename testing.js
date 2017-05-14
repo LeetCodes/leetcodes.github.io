f90a4c5e8c2a4b7659240b882737713c9558f733
LeetCodes <michael@leetcodes.com>
2017-05-14 17:06:50 -0400

    .
    .

--- testing.js
+++ testing.js
@@ -1,16 +1,1 @@
-infinity_scroll("loaded_content", "/top_rated/female/weekly", 1 );
-
-$.ajax({
-	type: "GET",
-	url: "/top_rated/female/weekly",
-	data: {
-		pg: 1
-	},
-	success: function(result) {
-		if (result) {
-			console.log(result);
-		} else {
-			var done_scrolling = 1;
-		}
-	}
-});
+alert(1);
